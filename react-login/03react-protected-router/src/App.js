35e6e75322a1f1c14c2dfcf31b92feee31026231
oneny <gjwodnjs1996@gmail.com>
2022-11-16 16:55:57 +0900

    Add: 로그인 후 react-router-dom을 통한 인증 및 라우트 보호

--- react-login/03react-protected-router/src/App.js
+++ react-login/03react-protected-router/src/App.js
@@ -1,12 +1,49 @@
-import Register from "./Register";
-import Login from "./Login";
+import Register from "./component/Register";
+import Login from "./component/Login";
+import Layout from "./component/Layout";
+import LinkPage from "./component/LinkPage";
+import Unauthroized from "./component/Unauthorized";
+import Home from "./component/Home";
+import Editor from "./component/Editor";
+import Admin from "./component/Admin";
+import Lounge from "./component/Lounge";
+import Missing from "./component/Missing";
+import ROLES from "./utils/ROLES";
+
+import { Routes, Route } from "react-router-dom";
+import RequireAuth from "./component/RequireAuth";
 
 function App() {
   return (
-    <main className="App">
-      <Login />
-      {/* <Register /> */}
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="linkpage" element={<LinkPage />} />
+        <Route path="unauthorized" element={<Unauthroized />} />
+
+        {/* we want to protect these routes */}
+        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />} >
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />} >
+          <Route path="/editor" element={<Editor />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />} >
+          <Route path="/admin" element={<Admin />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />} >
+          <Route path="/lounge" element={<Lounge />} />
+        </Route>
+
+        {/* catch call */}
+        <Route apth="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
