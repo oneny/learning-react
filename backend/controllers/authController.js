8181607884e7b81e21772822c8c4e4138865b211
oneny <gjwodnjs1996@gmail.com>
2022-11-16 14:25:48 +0900

    Feat: 로그인 후 받은 accessToken을 Context에 저장하는 기능 구현

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -18,7 +18,7 @@ const handleLogin = async (req, res) => {
   // evaluate password
   const match = await bcrypt.compare(pwd, foundUser.password);
   if (match) {
-    const roles = Object.values(foundUser.roles);
+    const roles = Object.values(foundUser.roles).filter(Boolean);
     // create JWTs
     const accessToken = jwt.sign(
       {
@@ -51,7 +51,7 @@ const handleLogin = async (req, res) => {
 
     // refrshToken은 쿠키로
     res.cookie("jwt", refreshToken, { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 }); // secure: true
-    res.json({ accessToken }); // accessToken은 json으로
+    res.json({ accessToken, roles }); // accessToken은 json으로
   } else {
     res.sendStatus(401); // Unauthorized
   }
