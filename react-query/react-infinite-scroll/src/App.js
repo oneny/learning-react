2bbff5695fca6d6180b58a02c6efa7d7b0df13a1
oneny <gjwodnjs1996@gmail.com>
2023-01-11 22:30:01 +0900

    add: react-query infinitescroll 구현

---
+++ react-query/react-infinite-scroll/src/App.js
@@ -0,0 +1,9 @@
+import Example2 from "./Example2";
+
+function App() {
+  return (
+    <Example2 />
+  );
+}
+
+export default App;
---
+++ react-query/react-infinite-scroll/src/Post.js
@@ -0,0 +1,19 @@
+import { forwardRef } from 'react'
+
+const Post = forwardRef(({ post }, ref) => {
+  const postBody = (
+    <>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <p>Post ID: {post.id}</p>
+    </>
+  );
+
+  const content = ref
+    ? <article ref={ref}>{postBody}</article>
+    : <article>{postBody}</article>
+
+  return content;
+});
+
+export default Post
