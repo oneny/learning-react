8786fd3eeda4fad807cf477ddf41b7c7686eec0a
oneny <gjwodnjs1996@gmail.com>
2023-01-26 12:19:41 +0900

    add: base-lazy-days setQueriesData, removeQueries, dependent Queries 추가

--- react-query/base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ react-query/base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,4 +1,5 @@
 import { AxiosResponse } from 'axios';
+import { useQuery, useQueryClient } from 'react-query';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -9,16 +10,18 @@ import {
   setStoredUser,
 } from '../../../user-storage';
 
-// async function getUser(user: User | null): Promise<User | null> {
-//   if (!user) return null;
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${user.id}`,
-//     {
-//       headers: getJWTHeader(user),
-//     },
-//   );
-//   return data.user;
-// }
+// 사용자의 로그인 여부에 따라 사용자 객체나 null이 될 수 있다.
+async function getUser(user: User | null): Promise<User | null> {
+  if (!user) return null;
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${user.id}`,
+    {
+      headers: getJWTHeader(user),
+    },
+  );
+
+  return data.user;
+}
 
 interface UseUser {
   user: User | null;
@@ -27,17 +30,34 @@ interface UseUser {
 }
 
 export function useUser(): UseUser {
+  const queryClient = useQueryClient();
   // TODO: call useQuery to update user data from server
-  const user = null;
+  // 기존 user의 값을 인자로 건네 user의 값을 업데이트하는데 사용된다.
+  const { data: user } = useQuery(queryKeys.user, () => getUser(user), {
+    // onSuccess는 쿼리 함수나 setQueryData에서 데이터를 가져오는 함수이다.
+    initialData: getStoredUser(),
+    onSuccess: (received: User | null) => {
+      if (!received) {
+        clearStoredUser();
+      } else {
+        setStoredUser(received);
+      }
+    },
+  });
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
     // TODO: update the user in the query cache
+    // 설정하려는 쿼리 데이터에 대한 키를 입력하고, newUser로 전달된 데이터를 지정
+    queryClient.setQueryData(queryKeys.user, newUser);
   }
 
   // meant to be called from useAuth
   function clearUser() {
     // TODO: reset user to null in query cache
+    // setQueriesData는 실행 후 onSuccess가 실행되고, removeQueires는 실행되지 않는다.
+    queryClient.setQueriesData(queryKeys.user, null);
+    queryClient.removeQueries('user-appointments');
   }
 
   return { user, updateUser, clearUser };
--- react-query/base-lazy-days/client/src/components/user/hooks/useUserAppointments.ts
+++ react-query/base-lazy-days/client/src/components/user/hooks/useUserAppointments.ts
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import { useQuery } from 'react-query';
 
 import type { Appointment, User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -6,17 +7,29 @@ import { queryKeys } from '../../../react-query/constants';
 import { useUser } from './useUser';
 
 // for when we need a query function for useQuery
-// async function getUserAppointments(
-//   user: User | null,
-// ): Promise<Appointment[] | null> {
-//   if (!user) return null;
-//   const { data } = await axiosInstance.get(`/user/${user.id}/appointments`, {
-//     headers: getJWTHeader(user),
-//   });
-//   return data.appointments;
-// }
+async function getUserAppointments(
+  user: User | null,
+): Promise<Appointment[] | null> {
+  // user.id가 없다면 서버에 연결을 시도하지 않도록 보수적으로 프로그래밍
+  if (!user) return null;
+  const { data } = await axiosInstance.get(`/user/${user.id}/appointments`, {
+    headers: getJWTHeader(user),
+  });
+  return data.appointments;
+}
 
 export function useUserAppointments(): Appointment[] {
   // TODO replace with React Query
-  return [];
+  const { user } = useUser();
+
+  const fallback: Appointment[] = [];
+  const { data: userAppointments = fallback } = useQuery(
+    'user-appointments',
+    () => getUserAppointments(user),
+    {
+      enabled: !!user,
+    },
+  );
+
+  return userAppointments;
 }
